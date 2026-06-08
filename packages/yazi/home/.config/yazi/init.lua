e78cafb71067ee95dba24a6e19562410064e2a32
Younes El Alami <shadoow.ma@gmail.com>
2026-06-08 03:19:16 +0100

    add yazi rga

--- packages/yazi/home/.config/yazi/init.lua
+++ packages/yazi/home/.config/yazi/init.lua
@@ -7,6 +7,25 @@ require('git'):setup()
 -- Relative number motions
 require('relative-motions'):setup({ show_numbers = 'relative', show_motion = true })
 
+-- fr.yazi search (rg + rga channels)
+require('fr'):setup({
+  rga = {
+    '--follow',
+    '--hidden',
+    '--no-ignore',
+    '--glob',
+    '\'!.git\'',
+    '--glob',
+    '\'!node_modules\'',
+    '--glob',
+    '\'!.venv\'',
+    '--glob',
+    '\'!venv\'',
+    '--glob',
+    '\'!__pycache__\'',
+  },
+})
+
 -- Bookmarks
 require('bookmarks'):setup({
   save_path = os.getenv('HOME') .. '/.local/share/yazi/bookmarks',
