7afc8813aa486e29334815e32c26f7981f9b5f68
Younes El Alami <younes.el-alami@bruce.work>
2026-06-19 15:03:21 +0100

    improve copy path

--- packages/nvim/home/.config/nvim/lua/plugins/tools/genghis.lua
+++ packages/nvim/home/.config/nvim/lua/plugins/tools/genghis.lua
@@ -1,5 +1,5 @@
 -- File operations from within the buffer (Genghis)
--- Keymaps: fn=new fe=new-in-folder fs=from-selection fd=dup fr=rename fm=move fx=chmod fD=trash fp=path fy=name
+-- Keymaps: fn=new fe=new-in-folder fs=from-selection fd=dup fr=rename fm=move fx=chmod fD=trash fp=path fy=rel-path fY=abs-path
 return {
   'chrisgrieser/nvim-genghis',
   event = 'VeryLazy',
@@ -23,10 +23,31 @@ return {
 
     -- Copy
     vim.keymap.set('n', '<leader>fY', '<cmd>Genghis copyFilepath<CR>', {
-      desc = 'Copy path',
+      desc = 'Copy absolute path',
     })
-    vim.keymap.set('n', '<leader>fy', '<cmd>Genghis copyFilename<CR>', {
-      desc = 'Copy name',
-    })
+    vim.keymap.set('n', '<leader>fy', function()
+      local filepath = vim.api.nvim_buf_get_name(0)
+      if filepath == '' then return end
+      local rel_path
+      -- diffview:///abs/path/.git/<hash>/relative/path
+      local diffview_inner = filepath:match('^diffview://(.+)$')
+      if diffview_inner then
+        rel_path = diffview_inner:match('/.git/[^/]+/(.+)$')
+        if not rel_path then
+          vim.notify('Could not parse diffview path', vim.log.levels.WARN, { title = 'Copy' })
+          return
+        end
+      else
+        local git_dir = vim.fn.finddir('.git', vim.fn.fnamemodify(filepath, ':h') .. ';')
+        local root = git_dir ~= '' and vim.fn.fnamemodify(git_dir, ':h') or nil
+        if root and root ~= '' then
+          rel_path = vim.fn.fnamemodify(filepath, ':p'):sub(#root + 2)
+        else
+          rel_path = vim.fn.fnamemodify(filepath, ':.')
+        end
+      end
+      vim.fn.setreg('+', rel_path)
+      vim.notify(rel_path, vim.log.levels.INFO, { title = 'Copied' })
+    end, { desc = 'Copy relative path' })
   end,
 }
